6d711e61c17f1bbcd3b045d70a2d5df387d9fb6e
ole kristensen <ole@kristensen.name>
2017-03-16 09:39:37 +0100

    Converted to ofDmx

--- src/fullScreenDialogs.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "ofSystemUtils.h"
-
-ofFileDialogResult fullScreenLoadDialog(string windowTitle="", bool bFolderSelection = false, string defaultPath="");
-ofFileDialogResult fullScreenSaveDialog(string defaultName, string messageName);
